dff4b198f4b77b2e8866049e51901889d5e4c20f
John <jackbull328@gmail.com>
2017-02-21 21:09:45 -0800

    changed port to 80

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/", function(req, res) {
   res.send("This will be the landing page!");
 });
 
-app.listen(process.env.PORT || 80, function() {
+app.listen(80, function() {
   console.log("The Modesto All Star Server is running");
 });
