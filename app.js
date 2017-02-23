71835109fb34885ea28ed4f3103f05262223d6e7
John <jackbull328@gmail.com>
2017-02-22 21:54:48 -0800

    Created Landing page, route and basic styles

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
     app = express();
 
+    app.set('views', __dirname + '/views');
+    app.set("view engine", "ejs");
+    app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
-  res.send("This will be the landing page!");
+  res.render("landing");
 });
 
 app.listen(process.env.PORT || 3000, function() {
