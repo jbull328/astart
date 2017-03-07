155fa9c7c569dc363dca2ab262aba8078e5a9bce
John <jackbull328@gmail.com>
2017-03-06 20:41:14 -0800

    Worked on getting file uploads to work

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     cloudinary = require("cloudinary"),
+    multer = require("multer"),
+    path = require('path');
+    upload = multer({ dest: 'https://res.cloudinary.com/jbull238/FCCAllStars/' }),
     app = express();
 
     app.set('views', __dirname + '/views');
     app.use(bodyParser.urlencoded({extended: true}));
     app.set("view engine", "ejs");
     app.use(express.static(__dirname + '/public'));
-    app.set(cloudinary.config({
+    cloudinary.config({
         cloud_name: 'jbull328',
         api_key: '339719788594166',
         api_secret: 'Mqqa4AFIRujSei3S7Ixb9DuRC4E'
-}));
+});
 
 mongoose.connect('mongodb://localhost/modestoFCCUsers');
 var modestoFCCUsers = new mongoose.Schema({
@@ -55,10 +58,9 @@ app.post('/user/new', function(req, res, next) {
   var currentOccupation = req.body.currentOccupation;
   var userEmail = req.body.userEmail;
   var description = req.body.description;
-  var userAvatar = req.files.avatar.path;
   var newUser = {fName: fName, lName: lName, description: description, userEmail: userEmail,};
     FccUsers.create(newUser, function(err, newlyCreatedUser) {
-      cloudinary.uploader.upload(userAvatar, function(result) {
+      cloudinary.uploader.upload('avatar', function(result) {
       console.log(result);
     if (err) {
       console.log(err);
